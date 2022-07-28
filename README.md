4613b5be9bff64535eda748f11160059e7bf48ab
Dominic Henry <dominichenrywork@hotmail.com>
2022-07-28 11:51:31 -0500

    Updates

---
+++ README.md
@@ -0,0 +1,3 @@
+## Creating Private Key
+
+[Create a Private Key and Self-Signed Digital Certificate](https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_auth_key_and_cert.htm)
--- index.ts
+++ index.ts
@@ -1,25 +1,63 @@
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
-// import { helmet } from 'fastify-helmet';
-import { Connection } from 'jsforce';
+import Fastify, { FastifyInstance } from 'fastify'
 import 'dotenv/config';
-import jwt, { Secret, SignOptions } from 'jsonwebtoken';
-import axios from 'axios';
 import jsforce from 'jsforce';
 import { getToken } from 'sf-jwt-token';
-// import { Server, IncomingMessage, ServerResponse } from 'http'
+import { isArray } from 'util';
 
 const server: FastifyInstance = Fastify({ logger: true })
 const conn = new jsforce.Connection({});
 
+type Order = {
+  Id: string,
+  AccountId: string, // shopper salesforceID
+  Delivery_Date__c: Date,
+}
+
+type CreateOrder = {
+  AccountId: string,
+  ProductIds: string[],
+  Delivery_Date__c: Date,
+}
+
 server.get('/shoppers', async () => {
-  const shoppers = await conn.query('SELECT Name, AccountNumber, BillingAddress, ShippingAddress FROM Account');
+  const shoppers = await conn.query('SELECT Id, Name, AccountNumber, BillingAddress, ShippingAddress FROM Account');
   return { shoppers };
 })
 
 server.get('/orders', async () => {
   // https://developer.salesforce.com/docs/atlas.en-us.order_management_developer_guide_html.meta/order_management_developer_guide_html/order_management_import_data.htm
-  const orders = await conn.query('SELECT Id, Account.Name, Status, Type, Delivery_Date__c, ShippingAddress, TotalAmount FROM Order');
-  return { orders };
+  const result = await conn.query('SELECT Id, Account.Name, Status, Type, Delivery_Date__c, ShippingAddress, TotalAmount, OrderNumber FROM Order');
+  const orderId = (result?.records as Order[])[0]?.Id;
+  const orderItems = await conn.query(`SELECT Product2Id, TotalPrice FROM OrderItem WHERE OrderId='${orderId}'`);
+  return { 
+    orders: {...result}, 
+    orderItems: {...orderItems}
+  };
+})
+
+server.get('/products', async () => {
+  const products = await conn.query('SELECT Id, Name FROM Product2');
+  return { products };
+})
+
+server.post('/orders', async (request, response) => {
+  const { 
+    AccountId, 
+    Delivery_Date__c,
+    ProductIds 
+  } = request.body as CreateOrder;
+
+  // Create new order
+  const result = await conn.sobject('Order').create([
+    {
+      AccountId,
+      Delivery_Date__c,
+      EffectiveDate: new Date(),
+      Status: 'Draft'
+    }
+  ])
+
+  return { ...result };
 })
 
 const start = async () => {
